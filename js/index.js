bd1d402fc8d7ce23ca6412692052e4ed521eb4ec
Teguh Permadi <tguh.permadi@gmail.com>
2017-09-26 10:06:11 +0700

    1.4.4

--- js/index.js
+++ js/index.js
@@ -25,10 +25,10 @@
  */
 
 var app = {
-    // global vars
+    global vars
     autoShowInterstitial: false,
-    //progressDialog: document.getElementById("progressDialog"),
-    //spinner: document.getElementById("spinner"),
+    progressDialog: document.getElementById("progressDialog"),
+    spinner: document.getElementById("spinner"),
     weinre: {
         enabled: false,
         ip: '', // ex. http://192.168.1.13
